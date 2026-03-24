c1decb7131b3c63d4bda3139cc31104eb9408a4e
jbatres-2025512 <jbatres-2025512@kinal.edu.gt>
2026-03-24 16:26:05 -0600

    feat: added Product entity by jbatres-2025512

---
+++ src/main/java/com/jhulianbatres/kinlapp/entity/Product.java
@@ -0,0 +1,81 @@
+package com.jhulianbatres.kinlapp.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Products")
+public class Product {
+
+    @Id
+    @Column(name = "product_code")
+    private String productCode;
+
+    @Column
+    private String productName;
+
+    @Column
+    private float price;
+
+    @Column
+    private int stock;
+
+    @Column
+    private int productState;
+
+    @OneToMany(mappedBy = "codeProduct",cascade = CascadeType.ALL)
+    private List<Product> saleDetail;
+
+
+    public Product() {
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getProductState() {
+        return productState;
+    }
+
+    public void setProductState(int productState) {
+        this.productState = productState;
+    }
+
+    public List<Product> getSaleDetail() {
+        return saleDetail;
+    }
+
+    public void setSaleDetail(List<Product> saleDetail) {
+        this.saleDetail = saleDetail;
+    }
+}
