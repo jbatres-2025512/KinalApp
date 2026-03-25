21b05f53e496ad2930501996acc49ceff11ccb0e
jbatres-2025512 <jbatres-2025512@kinal.edu.gt>
2026-03-25 14:33:52 -0600

    feat: has been added the UserRepository class by jbatres-2025512

---
+++ src/main/java/com/jhulianbatres/kinlapp/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.jhulianbatres.kinlapp.repository;
+
+import com.jhulianbatres.kinlapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <User,Long> {
+}
