b21e9ef862b8fbc341e2a0cded695d30c264468a
jbatres-2025512 <jbatres-2025512@kinal.edu.gt>
2026-03-25 14:29:56 -0600

    feat: has been added the SaleDetailRepository class by jbatres-2025512

---
+++ src/main/java/com/jhulianbatres/kinlapp/repository/SaleDetailRepository.java
@@ -0,0 +1,7 @@
+package com.jhulianbatres.kinlapp.repository;
+
+import com.jhulianbatres.kinlapp.entity.SaleDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleDetailRepository extends JpaRepository <SaleDetail,Long> {
+}
