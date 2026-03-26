2142de052cf40fe555cfc49b5a89726dae018322
jbatres-2025512 <jbatres-2025512@kinal.edu.gt>
2026-03-25 22:24:05 -0600

    feat: add GetMapping to list all users by jbatres-2025512

--- src/main/java/com/jhulianbatres/kinlapp/controller/UserController.java
+++ src/main/java/com/jhulianbatres/kinlapp/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.jhulianbatres.kinlapp.controller;
 
+import com.jhulianbatres.kinlapp.entity.User;
 import com.jhulianbatres.kinlapp.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,16 +11,22 @@ import java.util.List;
 
 @RestController
 @RequestMapping("{/users}")
-public class User {
+public class UserController {
 
     private final UserService userService;
 
-    public User(UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    
+    @GetMapping
+    public ResponseEntity<List<User>>list(){
+        List<User> users = userService.listAll();
 
+        return ResponseEntity.ok(users);
+    }
+
+    
 
 
 
