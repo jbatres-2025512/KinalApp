5a90b5aab54dd8cc639c7ad945d2f7a43f7c29a0
jbatres-2025512 <jbatres-2025512@kinal.edu.gt>
2026-03-25 22:36:08 -0600

    feat: add GetMapping to list users with usersCode by jbatres-2025512

--- src/main/java/com/jhulianbatres/kinlapp/controller/UserController.java
+++ src/main/java/com/jhulianbatres/kinlapp/controller/UserController.java
@@ -4,6 +4,7 @@ import com.jhulianbatres.kinlapp.entity.User;
 import com.jhulianbatres.kinlapp.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,18 @@ public class UserController {
         return ResponseEntity.ok(users);
     }
 
-    
+    @GetMapping("/{userCode}")
+    public ResponseEntity<User> searchByUserCode(@PathVariable Long userCode){
+
+        return userService.findByUserCode(userCode)
+
+                .map(ResponseEntity::ok)
+
+                .orElse(ResponseEntity.notFound().build());
+                    
+    }
+
+
 
 
 
