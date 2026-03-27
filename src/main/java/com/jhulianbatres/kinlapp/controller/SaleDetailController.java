eaa3f5d04f2af4cfb815911bfafd3c0e06651739
jbatres-2025512 <jbatres-2025512@kinal.edu.gt>
2026-03-27 16:01:34 -0600

    feat: add Get method on SaleDetailController class by jbatres-2025512

--- src/main/java/com/jhulianbatres/kinlapp/controller/SaleDetailController.java
+++ src/main/java/com/jhulianbatres/kinlapp/controller/SaleDetailController.java
@@ -1,10 +1,30 @@
 package com.jhulianbatres.kinlapp.controller;
 
+import com.jhulianbatres.kinlapp.entity.SaleDetail;
+import com.jhulianbatres.kinlapp.service.SaleDetailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/saleDetails")
 public class SaleDetailController {
 
+    private final SaleDetailService  saleDetailService;
 
+    public SaleDetailController(SaleDetailService saleDetailService) {
+        this.saleDetailService = saleDetailService;
+    }
 
+    @GetMapping
+    public ResponseEntity<List<SaleDetail>> list(){
+        List<SaleDetail> saleDetails = saleDetailService.listAll();
 
+        return ResponseEntity.ok(saleDetails);
+    }
 
-
+    
 }
