8174752e32002bd07b82f5602ca905524061cc35
jbatres-2025512 <jbatres-2025512@kinal.edu.gt>
2026-03-27 22:58:26 -0600

    feat: add Get method on ProductController

--- src/main/java/com/jhulianbatres/kinlapp/controller/ProductController.java
+++ src/main/java/com/jhulianbatres/kinlapp/controller/ProductController.java
@@ -1,4 +1,29 @@
 package com.jhulianbatres.kinlapp.controller;
 
+import com.jhulianbatres.kinlapp.entity.Product;
+import com.jhulianbatres.kinlapp.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>>listAll(){
+
+        List<Product> products = productService.listAll();
+
+        return ResponseEntity.ok(products);
+    }
+
 }
