fe592c4b9c3f57882b842680173a4028ed7893cc
jbatres-2025512 <jbatres-2025512@kinal.edu.gt>
2026-04-12 18:09:22 -0600

    feat: added a HomeWebController for home.html by jbatres-2025512

---
+++ src/main/java/com/jhulianbatres/kinlapp/controller/HomeWebController.java
@@ -0,0 +1,22 @@
+package com.jhulianbatres.kinlapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeWebController {
+
+
+
+    @GetMapping("/")
+    public String index(){
+
+
+
+
+        return "home";
+
+    }
+
+}
