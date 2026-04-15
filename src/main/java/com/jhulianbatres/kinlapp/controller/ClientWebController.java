8b25e03ea581a6fd47ef84f49366f753a4d25da2
jbatres-2025512 <jbatres-2025512@kinal.edu.gt>
2026-04-15 17:13:06 -0600

    feat: method to add clients on ClientWebController has been added by jbatres-2025512

--- src/main/java/com/jhulianbatres/kinlapp/controller/ClientWebController.java
+++ src/main/java/com/jhulianbatres/kinlapp/controller/ClientWebController.java
@@ -1,11 +1,16 @@
 package com.jhulianbatres.kinlapp.controller;
 
+import com.jhulianbatres.kinlapp.entity.Client;
 import com.jhulianbatres.kinlapp.service.ClientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/client")
 public class ClientWebController {
 
     public final ClientService clientService;
@@ -14,19 +19,25 @@ public class ClientWebController {
         this.clientService = clientService;
     }
 
-    @GetMapping("/client")
-    public String client(){
-        return ("client");
-    }
-
     @GetMapping
     public String clients(Model model){
 
-        model.addAttribute("DPIClient",clientService.listAll());
+        model.addAttribute("listClients",clientService.listAll());
 
         return ("client");
 
     }
+
+    @PostMapping("/add")
+    public String addClient(@ModelAttribute("client") Client client){
+
+        clientService.save(client);
+
+        return "redirect:/client";
+
+    }
+
+    
 
 
 
