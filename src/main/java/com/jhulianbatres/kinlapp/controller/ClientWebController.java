f0aef2d2ea9f9d33270fff30d8c4a1548023f6a2
jbatres-2025512 <jbatres-2025512@kinal.edu.gt>
2026-04-15 16:51:42 -0600

    feat: method to list all clients on ClientWebController has been added by jbatres-2025512

--- src/main/java/com/jhulianbatres/kinlapp/controller/ClientWebController.java
+++ src/main/java/com/jhulianbatres/kinlapp/controller/ClientWebController.java
@@ -1,14 +1,33 @@
 package com.jhulianbatres.kinlapp.controller;
 
+import com.jhulianbatres.kinlapp.service.ClientService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientWebController {
 
+    public final ClientService clientService;
+
+    public ClientWebController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
     @GetMapping("/client")
     public String client(){
         return ("client");
     }
+
+    @GetMapping
+    public String clients(Model model){
+
+        model.addAttribute("DPIClient",clientService.listAll());
+
+        return ("client");
+
+    }
+
+
 
 }
