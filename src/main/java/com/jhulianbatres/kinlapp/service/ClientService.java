ac02acc99f7cbc4e747ab0b42ed92a1da4568296
jambrocio-2025444 <Jambrocio-2025444@Kinal.edu.gt>
2026-05-12 17:21:38 -0600

    feat: the anotation @PreAuthorize for only ADMIN users for edit has been added by jbatres-2025512

--- src/main/java/com/jhulianbatres/kinlapp/service/ClientService.java
+++ src/main/java/com/jhulianbatres/kinlapp/service/ClientService.java
@@ -2,6 +2,7 @@ package com.jhulianbatres.kinlapp.service;
 
 import com.jhulianbatres.kinlapp.entity.Client;
 import com.jhulianbatres.kinlapp.repository.ClientRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -74,7 +75,9 @@ Que la clase contiene la logica del negocio
 
         }
 
+
         @Override
+        @PreAuthorize("hasRole('ADMIN')")
         public Client update(String DPIClient, Client client) {
 
             //Metodo para actualizar un cliente existente
